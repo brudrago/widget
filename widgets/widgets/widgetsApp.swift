b577cd0e0790bb6b4ec7f3a11861c441d22f25c6
Bruna Drago <bruna.drago@ifood.com.br>
2025-02-09 15:10:52 -0300

    Create project

---
+++ widgets/widgets/widgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  widgetsApp.swift
+//  widgets
+//
+//  Created by Bruna Drago on 09/02/25.
+//
+
+import SwiftUI
+
+@main
+struct widgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
